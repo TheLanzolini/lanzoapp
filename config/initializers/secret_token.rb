e0c84279dc2da81f16a364990d2399f8e493e3cf
TheLanzolini <thelanzolini@gmail.com>
2014-02-16 16:34:08 -0500

    fir REAL commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lanzoapp::Application.config.secret_key_base = 'fa62ef524943da97410154fc9b910277c06f5fca5a1fcc3ddcbb462ba047c6687515c870ade4cb7d64e64c83effa0c675a0abefafa07a1b1924c532894d7aab8'

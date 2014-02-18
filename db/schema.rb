da9b1addcc2f521d33e89f446def7ad833d14536
TheLanzolini <thelanzolini@gmail.com>
2014-02-17 21:28:43 -0500

    fixed migrating db

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217002757) do
-
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140218012749) do
 
   create_table "users", force: true do |t|
     t.string   "name"
